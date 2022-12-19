ea781af78fcba5f91632c9332608ec6d0f9b2a68
Admin operation TBDC <opradmintbdc@wms.bigc.co.th>
2022-12-19 09:28:43 +0700

    deploy to heroku

--- app.vue
+++ app.vue
@@ -3,7 +3,9 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-
+<style setup>
+@import url('https://fonts.googleapis.com/css2?family=Sarabun:wght@300&display=swap');
+</style>
 <script setup>
 if (process.client) {
   window.addEventListener('load', () => {
